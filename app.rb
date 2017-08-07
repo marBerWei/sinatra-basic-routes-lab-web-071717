8310b34b0d7f561b9e1cace435a1e973b1390c40
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-07 17:05:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get '/' do
+  "Hello, World!"
+end
+
+get '/name' do
+  "My name is Martina"
+end
+
+get '/hometown' do
+  "My hometown is Rockland"
+end
+
+get '/favorite-song' do
+  "My favorite song is 'Don't Stop Me Now'"
+end
+
 end
